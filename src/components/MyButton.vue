a8e56aa6b6b2941e4f3e75bcc6564051b6aab7ab
Tonny <musembitonnyj@gmail.com>
2022-08-08 22:02:34 +0300

    add

---
+++ src/components/MyButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <button>
+            This is MyButton
+        </button>
+    </div>
+</template>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+    <input type="text" placeholder="name" v-model="Name"/>
+    </div>
+    <div>
+        <!-- <button  @click="submitForm">Submit</button> -->
+    </div>
+    <h1 @click="handHeadClick">My Name is {{name}} an my age is {{age}}</h1>
+    <MyButton/>
+</template>
+<script setup>
+
+import { ref } from "vue";
+
+
+// import  MyButton from '@components/MyButton'
+//  const name = ref('');
+//  const SubmitForm = () => {console.log('FormSubmitted! Name = ${name.value}')
+//  }
+//  return{
+// name,
+// submitForm,
+//  },
+const name = ref('Tonny');
+const age =ref(26);
+
+const handHeadClick = ()=> {
+    name.value ='Steve'
+    age.value ='30'
+
+}
+
+</script>
