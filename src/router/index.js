a41400a557cdf4198bc9f77d483687790f00fd94
Tonny <musembitonnyj@gmail.com>
2022-08-25 16:01:29 +0300

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/components/Home.vue'
 import Login from "/src/views/login.vue";
+import Watchexample from '/src/components/watchexample.vue';
+
 const routes = [{
         path: '/',
         name: 'Home',
         component: Home,
     },
     { path: '/login', name: 'login', component: Login },
+    { path: '/watchexample', name: 'watchexample', component: Watchexample },
+
 
 
 
---
+++ src/views/login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+    <form @submit="login">
+        <input type="text" placeholder="enter name " v-bind="name"><br><br>
+        <input type="text" placeholder="enter password" v-bind="password"><br><br>
+        <button type="submit" class="btn btn-primary" v-on:click="login()">Login</button>
+        
+    </form>
+    </div>
+
+    <div>
+          <router-link to="/watchexample"> Home</router-link>
+
+    </div>
+</template>
+<script >
+export default {
+    data() {
+        return {
+            name:null,
+            password:null,
+
+        }
+    },
+    
+
+
+
+}
+methods: {
+    // login()
+    // {
+    //     // console.log("Login fuction called")
+
+
+    // }
+  
+    
+    
+}
+
+
+</script>
