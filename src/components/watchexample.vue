18f3a80926a7bccd28f059f61e5546cbbe1c10b5
Tonny <musembitonnyj@gmail.com>
2022-08-25 16:11:14 +0300

    add

---
+++ src/components/watchexample.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <h2>Watchers</h2>
+</div>
+<input type="text" v-model="Search" placeholder="Search">
+{{Search}}
+
+    
+</template>
+<script>
+import { ref } from 'vue';
+export default {
+    setup(props) {
+        const Search = ref("");
+         console.log('Search',Search.value)
+        // watch(Search,() =>   {
+        //  console.log('Search',Search.value)
+
+            
+        // }) 
+        
+    
+
+        return{
+            Search,
+
+        };
+        
+    }
+    
+
+
+
+}
+
+</script>
