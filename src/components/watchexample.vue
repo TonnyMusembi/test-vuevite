d8c2ba06f1510dc965d5b1c601cfb4934e537f06
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-02 11:15:21 +0300

    update

--- src/components/watchexample.vue
+++ src/components/watchexample.vue
@@ -5,14 +5,28 @@
 <input type="text" v-model="Search" placeholder="Search">
 {{Search}}
 
-<!-- <input type="text" v-model="input" placeholder="Search fruits..." />
-  <div class="item fruit" v-for="fruit in filteredList()" :key="fruit">
-    <p>{{ fruit }}</p>
-  </div>
-  <div class="item error" v-if="input&&!filteredList().length">
-     <p>No results found!</p>
-  </div> -->
-
+<div class="col-md-12 bg-light p-5 mb-5">
+<table class="table table-bordered">
+  <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+    
+        <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td scope="row">{{post.id}}</td>
+          <td>{{post.name}}</td>
+          <td>{{post.email}}</td>
+          <td><a href="#" class="btn btn-warning" @click="get_user(post.id,post.name,post.email)">Edit</a> <a href="#"  class="btn btn-danger" @click="delete_user(post.id)">Delete</a></td>
+         
+        </tr>
+      </tbody>
+</table>
+</div>
     
 </template>
 <script>
@@ -21,20 +35,7 @@ export default {
     setup(props) {
         const Search = ref("");
          console.log('Search',Search.value)
-//          let input = ref("");
 
-//          const fruits = ["apple", "banana", "orange"];
-// function filteredList() {
-//   return fruits.filter((fruit) =>
-//     fruit.toLowerCase().includes(input.value.toLowerCase())
-//   );
-// }
-
-        // watch(Search,() =>   {
-        //  console.log('Search',Search.value)
-
-            
-        // }) 
     
         return{
             Search,
@@ -42,10 +43,7 @@ export default {
         };
         
     }
-    
-
-
-
+  
 }
 
 </script>
