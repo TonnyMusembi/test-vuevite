e376bbbf02e80440eba1c1d1f26bf5ccc5c054a2
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-08 12:25:04 +0300

    add

--- src/components/watchexample.vue
+++ src/components/watchexample.vue
@@ -1,11 +1,11 @@
 <template>
-<div>
+  <div>
     <h2>Watchers</h2>
-</div>
-<input type="text" v-model="Search" placeholder="Search">
-{{Search}}
+  </div>
+  <input type="text" v-model="Search" placeholder="Search" />
+  {{ Search }}
 
-<!-- <input type="text" v-model="input" placeholder="Search fruits..." />
+  <!-- <input type="text" v-model="input" placeholder="Search fruits..." />
   <div class="item fruit" v-for="fruit in filteredList()" :key="fruit">
     <p>{{ fruit }}</p>
   </div>
@@ -13,39 +13,39 @@
      <p>No results found!</p>
   </div> -->
 
-    
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <datepicker name="birthdate"></datepicker>
+  </div>
 </template>
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
+import Datepicker from "vuejs-datepicker";
+
 export default {
-    setup(props) {
-        const Search = ref("");
-         console.log('Search',Search.value)
-//          let input = ref("");
+  components:{
+    Datepicker
+  },
+  setup(props) {
+    const Search = ref("");
+    console.log("Search", Search.value);
+    //          let input = ref("");
 
-//          const fruits = ["apple", "banana", "orange"];
-// function filteredList() {
-//   return fruits.filter((fruit) =>
-//     fruit.toLowerCase().includes(input.value.toLowerCase())
-//   );
-// }
+    //          const fruits = ["apple", "banana", "orange"];
+    // function filteredList() {
+    //   return fruits.filter((fruit) =>
+    //     fruit.toLowerCase().includes(input.value.toLowerCase())
+    //   );
+    // }
 
-        // watch(Search,() =>   {
-        //  console.log('Search',Search.value)
+    // watch(Search,() =>   {
+    //  console.log('Search',Search.value)
 
-            
-        // }) 
-    
-        return{
-            Search,
+    // })
 
-        };
-        
-    }
-    
-
-
-
-}
-
+    return {
+      Search,
+    };
+  },
+};
 </script>
