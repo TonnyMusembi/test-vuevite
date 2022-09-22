4de08e548c18213b5f486cd820e549b059e5b22e
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-22 23:52:45 +0300

    add

--- src/components/watchexample.vue
+++ src/components/watchexample.vue
@@ -17,10 +17,6 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <datepicker name="birthdate"></datepicker>
   </div>
-  
-
-
-  
 </template>
 <script>
 import { ref } from "vue";
@@ -46,11 +42,27 @@ export default {
     //  console.log('Search',Search.value)
 
     // })
+// getGames() {
+//       const payload = {
+//         params: { apiKey: "abcdefg1234567890", all: "true" },
+//           headers: {
+//             'X-RapidAPI-Key': '68a17b08dbmshe68484fe4f7905bp16ece9jsn2069f40c2728',
+//             'X-RapidAPI-Host': 'odds.p.rapidapi.com'}
+//       };
+
+//       const url = "https://odds.p.rapidapi.com/v1/sports";
+//       axios.get(url,payload).then((response) => {
+//         console.log(response.data.data);
+//         this.sports = response.data;
+//       });
+//     }
 
     return {
       Search,
       Datepicker,
     };
   },
+ 
+
 };
 </script>
