5ebb0d4c020169a3870dc043567a992f06a277a2
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-23 11:07:19 +0300

    add

---
+++ src/pages/cart/Cart.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="justify text-justify">
+        <button class="" onclick="(onCreate)">Create</button>
+
+    </div>
+</template>
+<script>
+   export default {
+    data() {
+        return {
+            data:[],
+            
+        }
+    },
+methods: {
+    onCreate(){
+        const url =""
+        axios.post('').then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        
+        })
+
+    }
+    
+},
+
+   }
+</script>
