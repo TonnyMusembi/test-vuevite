915f1bb788456dff03be568d7b69548a06b4d925
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-23 10:58:05 +0300

    add

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './tailwind.css'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "./tailwind.css";
 //import store from './store'
 
-import Vue3EasyDataTable from 'vue3-easy-data-table';
-import 'vue3-easy-data-table/dist/style.css';
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
-import router from "./router/index"
+import router from "./router/index";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
+app.use(Vue3EasyDataTable);
